03b204ec24cfd21f19abf7f7c2085ffc19e2c849
Broderick Stadden <broderick.stadden@xcentium.com>
2018-08-21 08:04:39 -0700

    changed api paths to relative paths

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT_GET: '"https://saasxm.xcentium.com/api/sitecore/dashboard/getsettings"',
-  API_ENDPOINT_SAVE: '"https://saasxm.xcentium.com/api/sitecore/dashboard/savechanges"'
+  API_ENDPOINT_GET: '"/api/sitecore/dashboard/getsettings"',
+  API_ENDPOINT_SUBMIT: '"/api/sitecore/dashboard/submitchanges"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ENDPOINT_GET: '"https://saasxm.xcentium.com/api/sitecore/dashboard/getsettings"',
-  API_ENDPOINT_SUBMIT: '"https://saasxm.xcentium.com/api/sitecore/dashboard/submitchanges"',
+  API_ENDPOINT_GET: '"/api/sitecore/dashboard/getsettings"',
+  API_ENDPOINT_SUBMIT: '"/api/sitecore/dashboard/submitchanges"',
   PUBLIC_ASSET_PATH: '/sitecore/shell/client/SaaS/Dashboard/' // Path to the application on sitecore, used to build resource paths
 }
